f00f6167edecaa7a00808b298479677946e3d476
Luc Georges <McPatate@users.noreply.github.com>
2023-09-23 16:32:52 +0200

    feat: integrate llm-ls (#70)
    * feat: add completions from llm-ls
    
    * docs: rename extension to
    
    * feat: cleanup & add attribution
    
    * fix: keep extension `name` the same to keep stats

--- src/configTemplates.ts
+++ src/configTemplates.ts
@@ -1,52 +1,75 @@
 const templateKeys = ["bigcode/starcoder", "codellama/CodeLlama-13b-hf", "Phind/Phind-CodeLlama-34B-v2", "WizardLM/WizardCoder-Python-34B-V1.0", "Custom"] as const;
 export type TemplateKey = typeof templateKeys[number];
 
-export interface Config {
-  modelIdOrEndpoint: string;
-  isFillMode: boolean;
-  autoregressiveModeTemplate: string;
-  fillModeTemplate: string;
-  temperature: number;
-  stopTokens: string[];
-  tokensToClear: string[];
+export interface TokenizerPathConfig {
+    path: string;
 }
 
-export const PREFIX = `[prefix]`;
-export const SUFFIX = `[suffix]`;
+export interface TokenizerRepoConfig {
+    repository: string;
+}
+
+export interface TokenizerUrlConfig {
+    url: string;
+    to: string;
+}
+
+export interface Config {
+    modelIdOrEndpoint: string;
+    "fillInTheMiddle.enabled": boolean;
+    "fillInTheMiddle.prefix": string;
+    "fillInTheMiddle.middle": string;
+    "fillInTheMiddle.suffix": string;
+    temperature: number;
+    contextWindow: number;
+    tokensToClear: string[];
+    tokenizer: TokenizerPathConfig | TokenizerRepoConfig | TokenizerUrlConfig | null;
+}
 
 const StarCoderConfig: Config = {
-  modelIdOrEndpoint: "bigcode/starcoder",
-  isFillMode: true,
-  autoregressiveModeTemplate: PREFIX,
-  fillModeTemplate: `<fim_prefix>${PREFIX}<fim_suffix>${SUFFIX}<fim_middle>`,
-  temperature: 0.2,
-  stopTokens: ["<|endoftext|>"],
-  tokensToClear: ["<fim_middle>"],
+    modelIdOrEndpoint: "bigcode/starcoder",
+    "fillInTheMiddle.enabled": true,
+    "fillInTheMiddle.prefix": "<fim_prefix>",
+    "fillInTheMiddle.middle": "<fim_middle>",
+    "fillInTheMiddle.suffix": "<fim_suffix>",
+    temperature: 0.2,
+    contextWindow: 8192,
+    tokensToClear: ["<|endoftext|>"],
+    tokenizer: {
+        repository: "bigcode/starcoder",
+    }
 }
 
 const CodeLlama13BConfig: Config = {
-  modelIdOrEndpoint: "codellama/CodeLlama-13b-hf",
-  isFillMode: true,
-  autoregressiveModeTemplate: PREFIX,
-  fillModeTemplate: `<PRE> ${PREFIX} <SUF>${SUFFIX} <MID>`,
-  temperature: 0.2,
-  stopTokens: ["<|endoftext|>", "<EOT>"],
-  tokensToClear: [ "<MID>"],
+    modelIdOrEndpoint: "codellama/CodeLlama-13b-hf",
+    "fillInTheMiddle.enabled": true,
+    "fillInTheMiddle.prefix": "<PRE> ",
+    "fillInTheMiddle.middle": " <MID>",
+    "fillInTheMiddle.suffix": " <SUF>",
+    temperature: 0.2,
+    contextWindow: 4096,
+    tokensToClear: ["<EOT>"],
+    tokenizer: {
+        repository: "codellama/CodeLlama-13b-hf",
+    }
 }
 
 const PhindCodeLlama34Bv2Config: Config = {
-  ...CodeLlama13BConfig,
-  modelIdOrEndpoint: "Phind/Phind-CodeLlama-34B-v2",
+    ...CodeLlama13BConfig,
+    modelIdOrEndpoint: "Phind/Phind-CodeLlama-34B-v2",
 }
 
 const WizardCoderPython34Bv1Config: Config = {
-  ...CodeLlama13BConfig,
-  modelIdOrEndpoint: "WizardLM/WizardCoder-Python-34B-V1.0",
+    ...CodeLlama13BConfig,
+    modelIdOrEndpoint: "WizardLM/WizardCoder-Python-34B-V1.0",
+    tokenizer: {
+        repository: "WizardLM/WizardCoder-Python-34B-V1.0",
+    }
 }
 
 export const templates: Partial<Record<TemplateKey, Config>> = {
-  "bigcode/starcoder": StarCoderConfig,
-  "codellama/CodeLlama-13b-hf": CodeLlama13BConfig,
-  "Phind/Phind-CodeLlama-34B-v2": PhindCodeLlama34Bv2Config,
-  "WizardLM/WizardCoder-Python-34B-V1.0": WizardCoderPython34Bv1Config,
+    "bigcode/starcoder": StarCoderConfig,
+    "codellama/CodeLlama-13b-hf": CodeLlama13BConfig,
+    "Phind/Phind-CodeLlama-34B-v2": PhindCodeLlama34Bv2Config,
+    "WizardLM/WizardCoder-Python-34B-V1.0": WizardCoderPython34Bv1Config,
 }
--- src/declarations.d.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-declare namespace Chai {
-  export interface LanguageChains {
-    shallowDeepEqual: (a: unknown) => void;
-  }
-}
