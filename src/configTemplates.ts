d2bda8730b629ba54dfa25725da77e6953ec6085
Luc Georges <McPatate@users.noreply.github.com>
2024-02-13 11:58:43 +0100

    feat: `0.5.x` (#129)
    * feat: `0.5.x`
    
    * docs: update README
    
    * feat: bump llm-ls to `0.5.2`

--- src/configTemplates.ts
+++ src/configTemplates.ts
@@ -1,75 +1,112 @@
-const templateKeys = ["bigcode/starcoder", "codellama/CodeLlama-13b-hf", "Phind/Phind-CodeLlama-34B-v2", "WizardLM/WizardCoder-Python-34B-V1.0", "Custom"] as const;
+const templateKeys = ["hf/bigcode/starcoder", "hf/codellama/CodeLlama-13b-hf", "hf/Phind/Phind-CodeLlama-34B-v2", "hf/WizardLM/WizardCoder-Python-34B-V1.0", "ollama/codellama:7b", "Custom"] as const;
 export type TemplateKey = typeof templateKeys[number];
 
 export interface TokenizerPathConfig {
-    path: string;
+	path: string;
 }
 
 export interface TokenizerRepoConfig {
-    repository: string;
+	repository: string;
 }
 
 export interface TokenizerUrlConfig {
-    url: string;
-    to: string;
+	url: string;
+	to: string;
 }
 
 export interface Config {
-    modelIdOrEndpoint: string;
-    "fillInTheMiddle.enabled": boolean;
-    "fillInTheMiddle.prefix": string;
-    "fillInTheMiddle.middle": string;
-    "fillInTheMiddle.suffix": string;
-    temperature: number;
-    contextWindow: number;
-    tokensToClear: string[];
-    tokenizer: TokenizerPathConfig | TokenizerRepoConfig | TokenizerUrlConfig | null;
+	modelId: string;
+	backend: "huggingface" | "ollama" | "openai" | "tgi";
+	url: string | null;
+	"fillInTheMiddle.enabled": boolean;
+	"fillInTheMiddle.prefix": string;
+	"fillInTheMiddle.middle": string;
+	"fillInTheMiddle.suffix": string;
+	requestBody: object;
+	contextWindow: number;
+	tokensToClear: string[];
+	tokenizer: TokenizerPathConfig | TokenizerRepoConfig | TokenizerUrlConfig | null;
 }
 
-const StarCoderConfig: Config = {
-    modelIdOrEndpoint: "bigcode/starcoder",
-    "fillInTheMiddle.enabled": true,
-    "fillInTheMiddle.prefix": "<fim_prefix>",
-    "fillInTheMiddle.middle": "<fim_middle>",
-    "fillInTheMiddle.suffix": "<fim_suffix>",
-    temperature: 0.2,
-    contextWindow: 8192,
-    tokensToClear: ["<|endoftext|>"],
+const HfStarCoderConfig: Config = {
+	modelId: "bigcode/starcoder",
+	backend: "huggingface",
+	url: null,
+	"fillInTheMiddle.enabled": true,
+	"fillInTheMiddle.prefix": "<fim_prefix>",
+	"fillInTheMiddle.middle": "<fim_middle>",
+	"fillInTheMiddle.suffix": "<fim_suffix>",
+	requestBody: {
+		parameters: {
+			max_new_tokens: 60,
+			temperature: 0.2,
+			top_p: 0.95
+		}
+	},
+	contextWindow: 8192,
+	tokensToClear: ["<|endoftext|>"],
+	tokenizer: {
+		repository: "bigcode/starcoder",
+	}
+}
+
+const HfCodeLlama13BConfig: Config = {
+	modelId: "codellama/CodeLlama-13b-hf",
+	backend: "huggingface",
+	url: null,
+	"fillInTheMiddle.enabled": true,
+	"fillInTheMiddle.prefix": "<PRE> ",
+	"fillInTheMiddle.middle": " <MID>",
+	"fillInTheMiddle.suffix": " <SUF>",
+	requestBody: {
+		parameters: {
+			max_new_tokens: 60,
+			temperature: 0.2,
+			top_p: 0.95
+		}
+	},
+	contextWindow: 4096,
+	tokensToClear: ["<EOT>"],
+	tokenizer: {
+		repository: "codellama/CodeLlama-13b-hf",
+	}
+}
+
+const HfPhindCodeLlama34Bv2Config: Config = {
+    ...HfCodeLlama13BConfig,
+    modelId: "Phind/Phind-CodeLlama-34B-v2",
+}
+
+const HfWizardCoderPython34Bv1Config: Config = {
+	...HfCodeLlama13BConfig,
+    modelId: "WizardLM/WizardCoder-Python-34B-V1.0",
     tokenizer: {
-        repository: "bigcode/starcoder",
+    	repository: "WizardLM/WizardCoder-Python-34B-V1.0",
     }
 }
 
-const CodeLlama13BConfig: Config = {
-    modelIdOrEndpoint: "codellama/CodeLlama-13b-hf",
-    "fillInTheMiddle.enabled": true,
-    "fillInTheMiddle.prefix": "<PRE> ",
-    "fillInTheMiddle.middle": " <MID>",
-    "fillInTheMiddle.suffix": " <SUF>",
-    temperature: 0.2,
-    contextWindow: 4096,
-    tokensToClear: ["<EOT>"],
-    tokenizer: {
-        repository: "codellama/CodeLlama-13b-hf",
-    }
-}
-
-const PhindCodeLlama34Bv2Config: Config = {
-    ...CodeLlama13BConfig,
-    modelIdOrEndpoint: "Phind/Phind-CodeLlama-34B-v2",
-}
-
-const WizardCoderPython34Bv1Config: Config = {
-    ...CodeLlama13BConfig,
-    modelIdOrEndpoint: "WizardLM/WizardCoder-Python-34B-V1.0",
-    tokenizer: {
-        repository: "WizardLM/WizardCoder-Python-34B-V1.0",
-    }
+const OllamaCodeLlama7BConfig: Config = {
+	...HfCodeLlama13BConfig,
+	modelId: "codellama:7b",
+	backend: "ollama",
+	url: "http://localhost:11434/api/generate",
+	requestBody: {
+		options: {
+			num_predict: 60,
+			temperature: 0.2,
+			top_p: 0.95
+		}
+	},
+	contextWindow: 2048,
+	tokenizer: {
+		repository: "codellama/CodeLlama-7b-hf",
+	}
 }
 
 export const templates: Partial<Record<TemplateKey, Config>> = {
-    "bigcode/starcoder": StarCoderConfig,
-    "codellama/CodeLlama-13b-hf": CodeLlama13BConfig,
-    "Phind/Phind-CodeLlama-34B-v2": PhindCodeLlama34Bv2Config,
-    "WizardLM/WizardCoder-Python-34B-V1.0": WizardCoderPython34Bv1Config,
+	"hf/bigcode/starcoder": HfStarCoderConfig,
+	"hf/codellama/CodeLlama-13b-hf": HfCodeLlama13BConfig,
+	"hf/Phind/Phind-CodeLlama-34B-v2": HfPhindCodeLlama34Bv2Config,
+	"hf/WizardLM/WizardCoder-Python-34B-V1.0": HfWizardCoderPython34Bv1Config,
+	"ollama/codellama:7b": OllamaCodeLlama7BConfig,
 }
